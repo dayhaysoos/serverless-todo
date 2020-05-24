0f4d7cc4c6f7c36cfb03d794aa129394d111959f
Nick DeJesus <ndejesus1227@gmail.com>
2020-05-24 19:57:45 -0400

    add query types

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -2,9 +2,39 @@ const { ApolloServer, gql } = require("apollo-server-lambda");
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    todos: [Todo]
+  }
+
+  type Todo {
+    id: ID!
+    text: String!
+    done: Boolean!
+  }
+
+  type Mutation {
+    addTodo(text: String!): Todo
+    updateTodoDone(id: ID!): Todo
   }
 `;
+
+const resolvers = {
+  Query: {
+    todos: () => {
+      return Object.values(todos);
+    },
+  },
+  Mutation: {
+    addTodo: (_, { text }) => {
+      todoIndex++;
+      const id = `key-${todoIndex}`;
+      todos[id] = { id, text, done: false };
+    },
+    updateTodoDone: (_, { id }) => {
+      todos[id].done = true;
+      return todos[id];
+    },
+  },
+};
 
 const resolvers = {
   Query: {
