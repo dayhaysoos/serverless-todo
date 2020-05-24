079a54210913aa3a37755d916f4eb08f4bf1cb53
Nick DeJesus <ndejesus1227@gmail.com>
2020-05-23 22:50:07 -0400

    todo logic

--- packages/www/src/components/dashboard.js
+++ packages/www/src/components/dashboard.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useRef, useContext, useReducer } from "react";
 import Layout from "./layout";
+import { Flex, Label, Input, Button, Checkbox } from "theme-ui";
+import { IdentityContext } from "../context/identity-context";
+
+const todosReducer = (state, action) => {
+  switch (action.type) {
+    case "addTodo":
+      return [...state, { done: false, value: action.payload }];
+    case "toggleTodoDone":
+      const newState = [...state];
+      newState[action.payload] = {
+        done: !state[action.payload].done,
+        value: state[action.payload].value,
+      };
+      return newState;
+  }
+};
 
 const Dashbaord = () => {
+  const { user, identity: netlifyIdentity } = useContext(IdentityContext);
+  const [todos, dispatch] = useReducer(todosReducer, []);
+  const inputRef = useRef();
+
   return (
     <Layout>
-      <h1>Dashboard</h1>
+      <Flex
+        as={"form"}
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "addTodo", payload: inputRef.current.value });
+          inputRef.current.value = "";
+        }}
+      >
+        <Label>
+          Add ToDo
+          <Input ref={inputRef} />
+        </Label>
+        <Button>Submit</Button>
+      </Flex>
+      <Flex sx={{ flexDirection: "column" }}>
+        {todos.map((todo, i) => (
+          <Flex as={"li"} key={i}>
+            <Checkbox checked={todo.done} />
+            <span>{todo.value}</span>
+          </Flex>
+        ))}
+      </Flex>
     </Layout>
   );
 };
