b46196366232779d43112df094ff8b122749f853
Nick DeJesus <ndejesus1227@gmail.com>
2020-06-19 18:25:26 -0400

    update url

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -23,8 +23,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const httpLink = new HttpLink({
-  uri:
-    "https://serverless-todo-netlify-fauna-egghead.netlify.com/.netlify/functions/graphql",
+  uri: "https://nick-serverless-todo.netlify.app/.netlify/functions/graphql",
 });
 const client = new ApolloClient({
   cache: new InMemoryCache(),
